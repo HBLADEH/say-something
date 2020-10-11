c80481e807db4b53730ef747391946f1e2f6255e
BLADE <1012582116@qq.com>
2020-10-11 23:53:12 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+impor
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  background-color: rgb(246, 246, 246);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ant from './utils/ant'
+
+
+createApp(App).use(ant).mount('#app')
